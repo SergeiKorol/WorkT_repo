b81c0da260fe3fd892ea14f8ba4a65449f1d124a
SergeiKorol <124100194+SergeiKorol@users.noreply.github.com>
2024-05-23 13:04:58 +0400

    расположение через Пайчарм

--- Work_team_2305/test/get_tasks_test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def get_tasks_test():
-    response = requests.get("https://todo-app-sky.herokuapp.com/")
-    assert response.status_code == 200
-    assert type(response.json()) is list
